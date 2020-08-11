f8a8c1ff516216904394e658eaa5e6cd2a481e30
João Brosin <joao.brosin@gmail.com>
2020-08-11 13:23:03 -0300

    content added

--- public/scripts/profile-modal.js
+++ public/scripts/profile-modal.js
@@ -1,3 +1,5 @@
+// to active modal
+
 const profileBox = document.querySelector('.profile-box')
 const profileModal = document.querySelector('.profile-modal')
 const closeButton = document.querySelector('.close-button')
@@ -9,4 +11,15 @@ profileBox.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
     profileModal.classList.remove('active')
 })
+
+// to change number of days in the profile-modal plan
+
+const endDay = document.querySelector('.end-day')
+const barSize = document.querySelector('.time-bar')
+
+const randomNumber = (Math.random() * 30 + 1).toFixed()
+const percentageNumber = (randomNumber / 3) * 10 + '%'
+
+endDay.innerHTML = randomNumber
+barSize.style.width = percentageNumber
 
